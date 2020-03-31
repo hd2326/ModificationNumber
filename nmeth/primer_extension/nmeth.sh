94a5129245f04fb7dfc0ed3429f69658c2108251
hd2326 <hd2326@columbia.edu>
2020-03-31 11:33:41 -0700

    Update nmeth.sh

--- nmeth/primer_extension/nmeth.sh
+++ nmeth/primer_extension/nmeth.sh
@@ -1,19 +1,24 @@
 #Code for data pre-processing
 
-/home/ubuntu/nanopolish/nanopolish index -d /brdu_controls_from_muller/primer_extension/fast5/ 2017_06_05_CAM_ONT_BrdU_PCRs_primer_ext_basecalled_guppy3.1.5.fastq &> index.log
+nanopolish=/home/ubuntu/nanopolish/nanopolish
+minimap2=/home/ubuntu/minimap2/minimap2
+samtools=/usr/bin/samtools
+#path to nanopolish, minimap2 and samtools
+
+$nanopolish index -d /path/to/fast5/file/folder/ /path/to/un-indexed/fastq/file &> index.log
 #indexing .fastq
 
-minimap2 -ax map-ont /brdu_controls_from_muller/primer_extension/ref.fa /brdu_controls_from_muller/primer_extension/fastq/2017_06_05_CAM_ONT_BrdU_PCRs_primer_ext_basecalled_guppy3.1.5.fastq -o primer_extension.sam &> minimap2.log
+$minimap2 -ax map-ont /path/to/reference/genome/file /path/to/indexed/fastq/file -o /path/to/sam/file -t 32 &> minimap2.log
 #alignment .fastq
 
-samtools view -b primer_extension.sam > primer_extension.bam
+$samtools view -b /path/to/sam/file > /path/to/bam/file
 #.sam to .bam
 
-samtools sort -o primer_extension.sorted.bam primer_extension.bam
+$samtools sort -o  /path/to/sorted/bam/file /path/to/bam/file
 #sorting .bam
 
-samtools index primer_extension.sorted.bam
+$samtools index /path/to/sorted/bam/file
 #indexing .bam
 
-/home/ubuntu/nanopolish/nanopolish eventalign -o /brdu_controls_from_muller/primer_extension/event/ -r 2017_06_05_CAM_ONT_BrdU_PCRs_primer_ext_basecalled_guppy3.1.5.fastq -b primer_extension.sorted.bam -g /brdu_controls_from_muller/primer_extension/ref.fa -t 18 --cigar_output --scale-events &> eventalign.log
+$nanopolish eventalign -o /path/to/event/table/folder/ -r /path/to/indexed/fastq/file -b /path/to/sorted/bam/file -g /path/to/reference/genome/file -t 32 --cigar_output --scale-events &> eventalign.log
 #event table generation
