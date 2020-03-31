95a28bd08cfb9bf4c1bfb6a73b0f90f26a09d325
hd2326 <hd2326@columbia.edu>
2020-03-31 11:32:13 -0700

    Update mrna.sh

--- mrna/mrna.sh
+++ mrna/mrna.sh
@@ -1,19 +1,24 @@
 #Code for data pre-processing
 
-/home/ubuntu/nanopolish/nanopolish index -d /home/ubuntu/mrna_na12878/ucsc_run1/fast5/ UCSC_Run1_20170907_DirectRNA.pass.dedup.fastq &> index.log
+nanopolish=/home/ubuntu/nanopolish/nanopolish
+minimap2=/home/ubuntu/minimap2/minimap2
+samtools=/usr/bin/samtools
+#path to nanopolish, minimap2 and samtools
+
+$nanopolish index -d /path/to/fast5/file/folder/ /path/to/un-indexed/fastq/file &> index.log
 #indexing .fastq
 
-minimap2 -ax map-ont /home/ubuntu/refGenome/Hs/fa/Homo_sapiens.GRCh38.dna.fa UCSC_Run1_20170907_DirectRNA.pass.dedup.fastq -o UCSC_Run1_20170907_DirectRNA.pass.dedup.sam -t 32 &> minimap2.log
+$minimap2 -ax map-ont /path/to/reference/genome/file /path/to/indexed/fastq/file -o /path/to/sam/file -t 32 &> minimap2.log
 #alignment .fastq
 
-samtools view -b UCSC_Run1_20170907_DirectRNA.pass.dedup.sam > UCSC_Run1_20170907_DirectRNA.pass.dedup.bam
+$samtools view -b /path/to/sam/file > /path/to/bam/file
 #.sam to .bam
 
-samtools sort -o  UCSC_Run1_20170907_DirectRNA.pass.dedup.sorted.bam UCSC_Run1_20170907_DirectRNA.pass.dedup.bam
+$samtools sort -o  /path/to/sorted/bam/file /path/to/bam/file
 #sorting .bam
 
-samtools index UCSC_Run1_20170907_DirectRNA.pass.dedup.sorted.bam
+$samtools index /path/to/sorted/bam/file
 #indexing .bam
 
-/home/ubuntu/nanopolish/nanopolish eventalign -o /home/ubuntu/mrna_na12878/ucsc_run1/event/ -r UCSC_Run1_20170907_DirectRNA.pass.dedup.fastq -b UCSC_Run1_20170907_DirectRNA.pass.dedup.sorted.bam -g /home/ubuntu/refGenome/Hs/fa/Homo_sapiens.GRCh38.dna.fa -t 32 --cigar_output --scale-events &> eventalign.log
+$nanopolish eventalign -o /path/to/event/table/folder/ -r /path/to/indexed/fastq/file -b /path/to/sorted/bam/file -g /path/to/reference/genome/file -t 32 --cigar_output --scale-events &> eventalign.log
 #event table generation
