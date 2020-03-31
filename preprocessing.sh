9bc41ca75d363da536d5ef85d48ff51dbe0e9bc8
hd2326 <hd2326@columbia.edu>
2020-03-31 11:37:57 -0700

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,7 +1,16 @@
+#Code for data pre-processing
+
 nanopolish=/home/ubuntu/nanopolish/nanopolish
+porechop=/home/ubuntu/anaconda3/bin/porechop
 minimap2=/home/ubuntu/minimap2/minimap2
 samtools=/usr/bin/samtools
-#path to nanopolish, minimap2 and samtools
+#path to nanopolish, porechop, minimap2 and samtools
+
+$nanopolish extract -v -r -q -t template -o /path/to/original/fastq/file /path/to/fast5/file/folder/ &> extract.log
+#extract .fastq from .fast5
+
+$porechop -i /path/to/original/fastq/file -b /path/to/demultiplexed/fastq/file/folder/ --untrimmed -t 12 &> porechop.log
+#demultiplexing .fastq
 
 $nanopolish index -d /path/to/fast5/file/folder/ /path/to/un-indexed/fastq/file &> index.log
 #indexing .fastq
